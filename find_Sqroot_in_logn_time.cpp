2e91c787c6df0e47cd3314286bd390253993680c
gitdhanshri <2022bit049@sggs.ac.in>
2024-02-05 11:12:20 +0530

    finding square root in time_complexity of (log n)

--- find_Sqroot_in_logn_time.cpp
+++ find_Sqroot_in_logn_time.cpp
@@ -6,20 +6,20 @@ int res=-1, low=1,high=x;
 while(low<=high){
 int mid=(low+high)/2;
 int root=mid*mid;
-if(root==x)
-	{
-	return mid;
-	}
-else if(root>x)
-	{
-	high=mid-1;
-	
-	}
-else
-	{
-	low=mid+1;
-	res=mid;
-	}
+		if(root==x)
+			{
+			return mid;
+			}
+		else if(root>x)
+			{
+			high=mid-1;
+			
+			}
+		else
+			{
+			low=mid+1;
+			res=mid;
+			}
 }
 return res;
 }
